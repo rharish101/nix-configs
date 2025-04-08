ac9895be64dbaf6443918f407308a038c5fe3fb6
Harish Rajagopal <harish.rajagopals@gmail.com>
2025-04-08 23:26:16 +0200

    Add snapper snapshots

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,43 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # Enable snapper for automatic Btrfs snapshots.
+  services.snapper.configs = {
+    persist = {
+      SUBVOLUME = "/persist";
+      TIMELINE_CREATE = true;
+      TIMELINE_CLEANUP = true;
+      TIMELINE_LIMIT_HOURLY = 0;
+      TIMELINE_LIMIT_DAILY = 5;
+      TIMELINE_LIMIT_WEEKLY = 0;
+      TIMELINE_LIMIT_MONTHLY = 5;
+      TIMELINE_LIMIT_QUARTERLY = 2;
+      TIMELINE_LIMIT_YEARLY = 2;
+    };
+    nix = {
+      SUBVOLUME = "/nix";
+      TIMELINE_CREATE = true;
+      TIMELINE_CLEANUP = true;
+      TIMELINE_LIMIT_HOURLY = 0;
+      TIMELINE_LIMIT_DAILY = 5;
+      TIMELINE_LIMIT_WEEKLY = 0;
+      TIMELINE_LIMIT_MONTHLY = 5;
+      TIMELINE_LIMIT_QUARTERLY = 0;
+      TIMELINE_LIMIT_YEARLY = 0;
+    };
+    home = {
+      SUBVOLUME = "/home";
+      TIMELINE_CREATE = true;
+      TIMELINE_CLEANUP = true;
+      TIMELINE_LIMIT_HOURLY = 5;
+      TIMELINE_LIMIT_DAILY = 5;
+      TIMELINE_LIMIT_WEEKLY = 0;
+      TIMELINE_LIMIT_MONTHLY = 5;
+      TIMELINE_LIMIT_QUARTERLY = 0;
+      TIMELINE_LIMIT_YEARLY = 0;
+    };
+  };
+
   # Enable locate.
   services.locate = {
     enable = true;
