cc21fd653ef85516f02217e048d252d50925862f
Harish Rajagopal <harish.rajagopals@gmail.com>
2025-04-10 17:06:13 +0200

    Remove now-unnecessary bcache-tools

--- configuration.nix
+++ configuration.nix
@@ -73,7 +73,6 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    bcache-tools
   ];
 
   # Enable fish globally to enable vendor completions.
