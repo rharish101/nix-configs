b8b44c8fe2553fcfd0053a03268c94f6866ccaf2
Harish Rajagopal <harish.rajagopals@gmail.com>
2025-04-14 09:37:23 +0200

    Add Eikar flags, CPU & memory limit to MC server

--- modules/containers/minecraft.nix
+++ modules/containers/minecraft.nix
@@ -2,6 +2,10 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+let
+  cpu_limit = 4;
+  memory_limit = 6; # in GiB
+in
 { ... }:
 {
   virtualisation.oci-containers.containers."minecraft" = {
@@ -12,9 +16,15 @@
       TYPE = "PAPER";
       VERSION = "1.21.5";
       PAPER_CHANNEL = "experimental";
+      MEMORY = "${toString memory_limit}G";
+      USE_AIKAR_FLAGS = "true";
     };
     volumes = [ "/data/minecraft:/data" ];
     podman.user = "minecraft";
+    extraOptions = [
+      "--memory=${toString (memory_limit + 1)}g" # Extra memory for some overhead
+      "--cpus=${toString cpu_limit}"
+    ];
   };
 
   # User for the Minecraft server.
