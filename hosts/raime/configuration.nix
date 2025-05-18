1f51bab3c20da6e3a736805e29ecc415a19cfb66
Harish Rajagopal <harish.rajagopals@gmail.com>
2025-05-18 13:27:01 +0200

    Enable firewall for raime

--- hosts/raime/configuration.nix
+++ hosts/raime/configuration.nix
@@ -30,10 +30,10 @@ in
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   networking.nat = {
     enable = true;
