781b5b0fac65a33ab21fa52d0384280eece83af9
Harish Rajagopal <harish.rajagopals@gmail.com>
2025-11-04 08:00:16 +0100

    Update inputs

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     impermanence.url = "github:nix-community/impermanence";
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.2";
+      url = "github:nix-community/lanzaboote/v0.4.3";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-minecraft = {
