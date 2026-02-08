8c33a290dab623aa4fed019bb95a85a8905f395d
Harish Rajagopal <harish.rajagopals@gmail.com>
2026-02-08 13:19:56 +0100

    Save snapshots for data dir

--- modules/services/snapper.nix
+++ modules/services/snapper.nix
@@ -44,5 +44,16 @@
         TIMELINE_LIMIT_QUARTERLY = 0;
         TIMELINE_LIMIT_YEARLY = 0;
       };
+      data = {
+        SUBVOLUME = "/data";
+        TIMELINE_CREATE = true;
+        TIMELINE_CLEANUP = true;
+        TIMELINE_LIMIT_HOURLY = 5;
+        TIMELINE_LIMIT_DAILY = 5;
+        TIMELINE_LIMIT_WEEKLY = 0;
+        TIMELINE_LIMIT_MONTHLY = 5;
+        TIMELINE_LIMIT_QUARTERLY = 2;
+        TIMELINE_LIMIT_YEARLY = 2;
+      };
     };
 }
